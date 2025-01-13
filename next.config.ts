12bbbca130a3324f66c5b2456f8b8cf982c80738
hhn2 <hwanghaena@gmail.com>
2025-01-13 13:50:40 -0500

    imagekit functioning

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: "placehold.co",
-      },
+
       {
         protocol:'https',
         hostname: "m.media-amazon.com",
+        port: "",
+      },
+      {
+        protocol:"https",
+        hostname:"ik.imagekit.io",
+        port:"",
       },
     ]
   }
